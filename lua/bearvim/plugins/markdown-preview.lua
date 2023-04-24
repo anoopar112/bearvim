e36f8d120ed73856cf6dbc5f4beb9b81cf4cf95a
Anoop Reghuvaran <anoop.reghuvaran@smart-steel-technologies.com>
2023-04-24 16:42:15 +0200

    Added the GFM css and modified markdown preview

--- lua/bearvim/plugins/markdown-preview.lua
+++ lua/bearvim/plugins/markdown-preview.lua
@@ -80,7 +80,7 @@ let g:mkdp_preview_options = {
 
 " use a custom markdown style must be absolute path
 " like '/Users/username/markdown.css' or expand('~/markdown.css')
-let g:mkdp_markdown_css = ''
+let g:mkdp_markdown_css = '~/.bearvim/lua/bearvim/assets/github-markdown.css'
 
 " use a custom highlight style must absolute path
 " like '/Users/username/highlight.css' or expand('~/highlight.css')
