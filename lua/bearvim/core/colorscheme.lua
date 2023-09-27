58c7c27ef9db737c923f8f134c09debb2a334d5f
Anoop Reghuvaran <anoop.reghuvaran@smart-steel-technologies.com>
2023-09-27 18:34:27 +0200

    updated

--- lua/bearvim/core/colorscheme.lua
+++ lua/bearvim/core/colorscheme.lua
@@ -11,10 +11,19 @@
 -- " \}
 -- ]])
 
+require("nightfox").setup({
+	options = {
+		styles = {
+			comments = "italic",
+			keywords = "bold",
+			types = "italic,bold",
+		},
+	},
+})
 local status, _ = pcall(vim.cmd, "colorscheme nightfox")
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
 	return
 end
 -- vim.cmd([[ highlight Normal guibg=None ]])
-vim.api.nvim_set_hl(0, "Comment", { italic = true })
+-- vim.api.nvim_set_hl(0, "Comment", { italic = true })
