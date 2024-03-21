a82d8c2e63057c22bab13ec680c941aa1c45b70b
Anoop Reghuvaran <anoop.reghuvaran@smart-steel-technologies.com>
2024-03-21 23:14:44 +0100

    updated markdown and vimtasks

--- lua/bearvim/plugins/vimtasks.lua
+++ lua/bearvim/plugins/vimtasks.lua
@@ -2,4 +2,5 @@
 vim.cmd([[
 let g:TasksDateFormat = '%Y-%m-%d %H:%M'
 autocmd BufNewFile,BufReadPost todo.md,doing.md,done.md set filetype=tasks
+autocmd BufWritePost todo.md,doing.md,done.md !mvTodos
 ]])
