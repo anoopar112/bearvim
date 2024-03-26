e9e9bba8df87075a8789445b9b2c497600f38ed1
Anoop Reghuvaran <anoop.reghuvaran@smart-steel-technologies.com>
2024-03-26 10:19:57 +0100

    Added multiple filetypes to md files

--- lua/bearvim/plugins/vimtasks.lua
+++ lua/bearvim/plugins/vimtasks.lua
@@ -1,6 +1,7 @@
 -- vimtasks configuration
 vim.cmd([[
 let g:TasksDateFormat = '%Y-%m-%d %H:%M'
-autocmd BufNewFile,BufReadPost todo.md,doing.md,done.md set filetype=tasks
+autocmd BufNewFile,BufReadPost *.md set filetype=tasks.vimwiki
 autocmd BufWritePost todo.md,doing.md,done.md !mvTodos && upwiki
+autocmd BufWritePost *.md !upwiki 
 ]])
